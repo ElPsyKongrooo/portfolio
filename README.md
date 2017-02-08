bbb251df20a22723374deb29b2d6a8ae1af9fb2b
ElPsyKongroo <igloriasha@gmail.com>
2017-02-08 14:09:35 +0800

    My Portfolio App

---
+++ README.md
@@ -0,0 +1,3 @@
+Sharlyn Igloria
+
+Portfolio
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+  def contact
+  end
+
+  def hire
+  end
+
+  def home
+  end
+
+  def projects
+  end
+
+  def skillsets
+  end
+
+
+end
